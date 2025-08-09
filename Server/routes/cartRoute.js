9985a237beebbd0ccf3bb3b4563971c3e45f5f7b
PranavThorat1432 <pranavthorat95@gmail.com>
2025-08-09 19:37:26 +0530

    Cart Route

---
+++ Server/routes/cartRoute.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import { addToCart, getUserCart, updateCart } from '../controllers/cartController.js';
+import authUser from '../middlewares/Auth.js';
+
+const cartRouter = express.Router();
+
+// Add Products to User cart
+// @API :- /api/cart/add-to-cart
+cartRouter.post('/add-to-cart', authUser, addToCart);
+
+// Update User cart
+// @API :- /api/cart/update-cart
+cartRouter.post('/update-cart', authUser, updateCart);
+
+// Get User cart data
+// @API :- /api/cart/get-cart
+cartRouter.post('/get-cart', authUser, getUserCart);
+
+export default cartRouter;
